c4e2c28e586b10a391309163817fff5b2cacb464
Scott Evans <git@scott.ee>
2014-07-19 21:09:31 +0100

    Only alerts to low temps in the evening, small refinements & code comments

--- nimbus.ino
+++ nimbus.ino
@@ -1,3 +1,5 @@
+// TO-DO: Consider MDNS - https://github.com/krvarma/MDNS_SparkCore
+
 // include the math library
 #include <math.h>
 
@@ -9,12 +11,13 @@ int ga1a = A0;
 
 // general vars
 #define MAXTIMINGS 85 // timings for DHT22
-uint8_t data[5]; 
-float rawRange = 4095; // analogRead returns value up to 4095 (http://docs.spark.io/#/firmware/i-o-analogread)
+uint8_t data[5];
+float rawRange = 4095; // analogRead returns values up to 4095 (http://docs.spark.io/#/firmware/i-o-analogread)
 float logRange = 5.0; //  3.3v = 10^5 lux (https://learn.adafruit.com/adafruit-ga1a12s202-log-scale-analog-light-sensor/use-it)
-int warningTempLow = 5; // enable warning when temperature reaches this value
+int warningTempLow = 6; // enable warning when temperature reaches this value
 #define ONE_DAY_MILLIS (24 * 60 * 60 * 1000) // a day in in millis
 unsigned long lastSync = millis(); // last sync time for time sync
+char json[64]; // to store JSON string
 
 // cloud vars
 char ip[24];
@@ -24,76 +27,84 @@ int rawlight = 0;
 double lux = 0;
 
 // set to true to debug over serial
-bool debug = false; 
+bool debug = false;
 
 // setup
 void setup() {
-    
+
     // debug over serial
     if (debug) {
         Serial.begin(9600);
-        while(!Serial.available()) {
-           Serial.println("Press any key to start...");
-           delay(2000);
-        }
-        Serial.println("Booting nimbus v1.0");
+	    while(!Serial.available()) {
+	       Serial.println("Press any key to start...");
+	       delay(2000);
+	    }
+    	Serial.println("Booting nimbus v1.0");
     }
 
-    // grab local IP
+	// grab local IP
     IPAddress nim_ip = Network.localIP();
     sprintf(ip, "%d.%d.%d.%d", nim_ip[0], nim_ip[1], nim_ip[2], nim_ip[3]);
-    
+
     // output IP over serial
-    if (debug) {
+	if (debug) {
         Serial.println(ip);
-        Serial.println("----------");
-    }
-    
+	    Serial.println("----------");
+	}
+
     // set led as output (http://docs.spark.io/#/firmware/setup-pinmode)
     pinMode(led, OUTPUT);
     pinMode(intled, OUTPUT);
-    
+
     // set dh22 as input
     pinMode(dht22, INPUT);
-    
+
     // setup cloud vars - currently disabled in favour of publish
     // Spark.variable("temperature", &temp, DOUBLE);
     // Spark.variable("humidity", &humidity, DOUBLE);
     // Spark.variable("lux", &lux, DOUBLE);
     // Spark.variable("ip", ip, STRING);
-    
+
     // turn off the bright led (save power and use it for low temp warning)
     if (!debug) {
         RGB.control(true);
         RGB.color(0, 0, 0);
     }
-    
+
     // delay
     delay(2000);
-    
+
     // read sensors
     readAllSensors();
-    
+
     // power throttle the wifi (https://community.spark.io/t/wifi-power-throttling/4486)?
 
-} 
+}
 
 // loop
 void loop() {
-  
+
     // what minute is it? - only do this 4 times per hour
     if ( Time.minute() == 00 || Time.minute() == 15 || Time.minute() == 30 || Time.minute() == 45) {
-    
+
         // read sensors
         readAllSensors();
-        
-        // publish (http://docs.spark.io/#/firmware/data-and-control-spark-publish)
-        Spark.publish("nimbus/temperature", String(temp), 60, PRIVATE);
-        Spark.publish("nimbus/humidity", String(humidity), 60, PRIVATE);
-        Spark.publish("nimbus/lux", String(lux), 60, PRIVATE);
-        Spark.publish("nimbus/ip", ip, 60, PRIVATE);
 
-        // output vars over serial when debuging 
+        // publish as single JSON even (http://docs.spark.io/#/firmware/data-and-control-spark-publish)
+        // there is a limit to the number of bytes (63) so shorter variable names are important
+        sprintf(json,"{\"t\": %.2f, \"h\": %.2f, \"l\": %.2f}", temp, humidity, lux);
+        Spark.publish("nimbus", json, 60, PRIVATE);
+
+        // old method of publishing seperate events for each variable
+        //Spark.publish("nimbus/temperature", String(temp), 60, PRIVATE);
+        //Spark.publish("nimbus/humidity", String(humidity), 60, PRIVATE);
+        //Spark.publish("nimbus/lux", String(lux), 60, PRIVATE);
+        //Spark.publish("nimbus/ip", ip, 60, PRIVATE);
+
+        // TO-DO. Make a local request to log data
+        // https://gist.github.com/wgbartley/0724c8def5c671c797bf
+
+        // output vars over serial when debuging
         if (debug) {
             Serial.println(temp);
             Serial.println(humidity);
@@ -101,27 +112,29 @@ void loop() {
             Serial.println(lux);
             Serial.println("----------");
         }
-    
+
         //  warn when low temperature
         if (temp <= warningTempLow) {
-        
-            // set on board LED to red & switch LED pin high
-            // RGB.color(255, 0, 0);
-            digitalWrite(led, HIGH);
-        
+
+            // set on board LED to red & switch LED pin high (at night 5pm to 11:59pm)
+            if (Time.hour() >= 17) {
+                RGB.color(255, 0, 0);
+                digitalWrite(led, HIGH);
+            }
+
             // publish (http://docs.spark.io/#/firmware/data-and-control-spark-publish)
             Spark.publish("nimbus/low", String(temp), 60, PRIVATE);
-        
-            // print to serial if debugging 
+
+            // print to serial if debugging
             if (debug) {
                 Serial.println("Low temperature warning!");
                 Serial.println("----------");
             }
 
         } else {
-        
+
             // turn off on board LED and switch LED pin low
-            // RGB.color(0, 0, 0);
+            RGB.color(0, 0, 0);
             digitalWrite(led, LOW);
         }
 
@@ -146,22 +159,22 @@ void loop() {
         delay(200);
         digitalWrite(intled, LOW);
         delay(200);
-        
+
         // sync the time from Spark Cloud ?
         if (millis() - lastSync > ONE_DAY_MILLIS) {
             Spark.syncTime();
             lastSync = millis();
         }
-        
+
         // delay for the rest of this minute as we only want one reading
         // this also gives us a whole minute to flash the core if necessary
-        delay(59000);
-        
-        // sleep the core to save power when not debugging 
-        if (!debug) {        
-            
-            // deep sleep the core for 13 minutes - we need to give it time to re-boot & re-connect
-            Spark.sleep(SLEEP_MODE_DEEP, 780);
+        delay(59000); // 59000
+
+        // sleep the core to save power when not debugging
+        if (!debug) {
+
+            // deep sleep the core for 14 minutes - we need to give it time to re-boot & re-connect
+            Spark.sleep(SLEEP_MODE_DEEP, 840);
 
             // if any sensors are attached in future that need constant monitoring (PIR?) then deep sleep needs to be disabled
             // to do this nimbus will need to use WiFi.on() and WiFi.off to get the data sent asap
@@ -169,33 +182,30 @@ void loop() {
             // Spark.sleep(780);
         }
     }
-    
-
-
 }
 
 /**
  * readAllSensors()
- * 
+ *
  * Wrapper function to read all sensors attached to nimbus
- * 
+ *
  * @return void
  */
 void readAllSensors() {
- 
-    // grab the lux value 
+
+    // grab the lux value
     rawlight = analogRead(ga1a);
     lux = readGA1A(rawlight);
-    
+
     // read the dht22 (temp & humidity)
-    readDHT22();   
+    readDHT22();
 }
 
 /**
  * readGA1A()
- * 
+ *
  * Convert the raw reading from the GA1A (http://www.adafruit.com/products/1384) to a lux value
- * 
+ *
  * @return float
  */
 double readGA1A(int raw) {
@@ -205,13 +215,13 @@ double readGA1A(int raw) {
 
 /**
  * readDHT22()
- * 
+ *
  * Read both the temperature and humidity with each read of the DHT22
- * 
+ *
  * @return void
  */
 void readDHT22(void) {
-    
+
     if (read()) {
         temp = readTemperature();
         humidity = readHumidity();
@@ -220,54 +230,54 @@ void readDHT22(void) {
 
 /**
  * readTemperature()
- * 
+ *
  * Calculate temperature based on data returned from read()
- * 
+ *
  * @return double
  */
 double readTemperature(void) {
-    
+
     double _t;
-    
+
     _t = data[2] & 0x7F;
     _t *= 256;
     _t += data[3];
     _t /= 10;
-                
+
     if (data[2] & 0x80)
         _t *= -1;
-                
+
     return _t;
 }
 
 /**
  * readHumdity()
- * 
+ *
  * Calculate humidity based on data returned from read()
- * 
+ *
  * @return double
  */
 double readHumidity(void) {
-    
+
     double _h;
-    
+
     _h = data[0];
     _h *= 256;
     _h += data[1];
     _h /= 10; // ?
-        
+
     return _h;
 }
 
 /**
  * read()
- * 
+ *
  * Read data from the DH22 as discussed here: https://community.spark.io/t/get-dht22-pulse-timing-with-micros/2689/3
- * 
+ *
  * @return bool
  */
 bool read(void) {
-    
+
     uint8_t i, j=0;
     uint8_t lastState = HIGH;
     uint8_t newState = HIGH;
